84f24051becbb46c97167f769145dada2a66298a
Raymond van der Gugten <montoo94@gmail.com>
2017-04-03 11:50:07 +0200

    bugfix

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,29 +1,28 @@
 app.controller('MainController', function($scope, LoginFactory){
-    var achievementList = [];
-    if(localStorage.getItem("achievementList") === null) {
-        achievementList = [
-            {
-                goal: 'get 100 points',
-                users: [{
+    var achievementList = [{
+        goal: 'get 100 points',
+        users: [{
+            username: 'Test'
+        }]
+    },
+        {
+            goal: 'get 1000 points',
+            users: [
+                {
                     username: 'Test'
-                }]
-            },
-            {
-                goal: 'get 1000 points',
-                users: [
-                    {
-                        username: 'Test'
-                    }
-                ]
-            },
-            {
-                goal: 'get 10000 points',
-                users: []
-            }
-        ];
+                }
+            ]
+        },
+        {
+            goal: 'get 10000 points',
+            users: []
+        }
+    ];
+    if(localStorage.getItem("achievementList") === null) {
+        localStorage.setItem("achievementList", JSON.stringify(achievementList));
     }
 
-        localStorage.setItem("achievementList", JSON.stringify(achievementList));
+
   $scope.title = "The best game in the world!";
   $scope.loginCheck = LoginFactory.loginCheck;
   console.log("MainTest");
