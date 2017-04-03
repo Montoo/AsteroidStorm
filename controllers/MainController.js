462b4a06a5a2a605f95ea03a7764c9b0fb714e58
Raymond van der Gugten <montoo94@gmail.com>
2017-04-03 11:38:12 +0200

    bugfix

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,4 +1,29 @@
 app.controller('MainController', function($scope, LoginFactory){
+    var achievementList = [];
+    if(localStorage.getItem("achievementList") === null) {
+        achievementList = [
+            {
+                goal: 'get 100 points',
+                users: [{
+                    username: 'Test'
+                }]
+            },
+            {
+                goal: 'get 1000 points',
+                users: [
+                    {
+                        username: 'Test'
+                    }
+                ]
+            },
+            {
+                goal: 'get 10000 points',
+                users: []
+            }
+        ];
+    }
+
+        localStorage.setItem("achievementList", JSON.stringify(achievementList));
   $scope.title = "The best game in the world!";
   $scope.loginCheck = LoginFactory.loginCheck;
   console.log("MainTest");
--- filters/AchievementFilter.js
+++ filters/AchievementFilter.js
@@ -1,31 +1,5 @@
 app.filter("AchievementFilter", function () {
     return function (user, achievementName) {
-        var achievementList = [];
-        if(localStorage.getItem("achievementList") === null) {
-            achievementList = [
-                {
-                    goal: 'get 100 points',
-                    users: [{
-                        username:'Test'
-                    }]
-                },
-                {
-                    goal: 'get 1000 points',
-                    users: [
-                        {
-                            username:'Test'
-                        }
-                    ]
-                },
-                {
-                    goal: 'get 10000 points',
-                    users: [
-                    ]
-                }
-            ];
-
-            localStorage.setItem("achievementList", JSON.stringify(achievementList));
-        }
         achievementList = JSON.parse(localStorage.getItem("achievementList"));
         var completed = false;
         achievementList.forEach(function (e) {
